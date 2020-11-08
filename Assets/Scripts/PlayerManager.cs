a28f5b784620f4700737c7467fbba73753fdf2c9
Thai Nguyen <nqcuong2@gmail.com>
2020-11-08 13:11:20 -0600

    Hide fake stone on hand after throwing & fixed camera viewport

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] GameObject rockPrefab;
+    [SerializeField] GameObject fakeHandRock;
     [SerializeField] Transform rightHandTransform;
     [SerializeField] int throwForce = 1000;
 
+    public static PlayerManager Instance { get; private set; }
     public static AudioSource PlayerAudioSource { get; private set; }
 
     private const string THROW_STATE = "Throw";
@@ -16,6 +18,9 @@ public class PlayerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (!Instance)
+            Instance = this;
+
         animator = GetComponent<Animator>();
         PlayerAudioSource = GetComponent<AudioSource>();
     }
@@ -23,7 +28,7 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && fakeHandRock.activeSelf)
         {
             animator.Play(THROW_STATE);
         }
@@ -31,8 +36,17 @@ public class PlayerManager : MonoBehaviour
 
     public void ThrowRock()
     {
-        var rock = Instantiate(rockPrefab);
-        rock.transform.position = rightHandTransform.position;
-        rock.GetComponent<Rigidbody>().AddRelativeForce(Camera.main.transform.forward * throwForce);
+        if (fakeHandRock.activeSelf)
+        {
+            var rock = Instantiate(rockPrefab);
+            rock.transform.position = rightHandTransform.position;
+            rock.GetComponent<Rigidbody>().AddRelativeForce(Camera.main.transform.forward * throwForce);
+            fakeHandRock.SetActive(false);
+        }
+    }
+
+    public void AllowThrowingRock()
+    {
+        fakeHandRock.SetActive(true);
     }
 }
