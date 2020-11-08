b8ab5d20fae9d94ed4cbc44e3c60cdccee5ce86f
Thai Nguyen <nqcuong2@gmail.com>
2020-11-07 19:02:25 -0600

    Player can now throw rocks

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] GameObject rockPrefab;
+    [SerializeField] Transform rightHandTransform;
+    [SerializeField] int throwForce = 1000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var rock = Instantiate(rockPrefab);
+            rock.transform.position = rightHandTransform.position;
+            rock.GetComponent<Rigidbody>().AddRelativeForce(Camera.main.transform.forward * throwForce);
+        }
+    }
+}
